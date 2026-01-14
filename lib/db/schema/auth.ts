2ae0d8c8a2340754630c8463a512eb35f209747d
sergsagan <sergey.martynuk@gmail.com>
2026-01-14 18:14:45 +0100

    auth setup

---
+++ lib/db/schema/auth.ts
@@ -0,0 +1,85 @@
+import { relations, sql } from "drizzle-orm";
+import {sqliteTable, text, integer, index, int} from "drizzle-orm/sqlite-core";
+
+export const user = sqliteTable("user", {
+  id: int().primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  email: text().notNull().unique(),
+  emailVerified: integer({mode: 'boolean'})
+    .notNull(),
+  image: text(),
+  createdAt: integer()
+    .notNull(),
+  updatedAt: integer()
+    .notNull(),
+});
+
+export const session = sqliteTable(
+  "session",
+  {
+    id: int().primaryKey({ autoIncrement: true }),
+    expiresAt: integer().notNull(),
+    token: text().notNull().unique(),
+    createdAt: integer()
+      .notNull(),
+    updatedAt: integer()
+      .notNull(),
+    ipAddress: text(),
+    userAgent: text(),
+    userId: text()
+      .notNull()
+      .references(() => user.id, { onDelete: "cascade" }),
+  },
+);
+
+export const account = sqliteTable(
+  "account",
+  {
+    id: int().primaryKey({ autoIncrement: true }),
+    accountId: text().notNull(),
+    providerId: text().notNull(),
+    userId: text()
+      .notNull()
+      .references(() => user.id, { onDelete: "cascade" }),
+    accessToken: text(),
+    refreshToken: text(),
+    idToken: text(),
+    accessTokenExpiresAt: integer(),
+    refreshTokenExpiresAt: integer(),
+    scope: text(),
+    password: text(),
+    createdAt: integer().notNull(),
+    updatedAt: integer().notNull(),
+  },
+);
+
+export const verification = sqliteTable(
+  "verification",
+  {
+    id: int().primaryKey({ autoIncrement: true }),
+    identifier: text().notNull(),
+    value: text().notNull(),
+    expiresAt: integer().notNull(),
+    createdAt: integer().notNull(),
+    updatedAt: integer().notNull(),
+  },
+);
+
+export const userRelations = relations(user, ({ many }) => ({
+  sessions: many(session),
+  accounts: many(account),
+}));
+
+export const sessionRelations = relations(session, ({ one }) => ({
+  user: one(user, {
+    fields: [session.userId],
+    references: [user.id],
+  }),
+}));
+
+export const accountRelations = relations(account, ({ one }) => ({
+  user: one(user, {
+    fields: [account.userId],
+    references: [user.id],
+  }),
+}));
