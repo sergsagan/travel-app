110cd06a227a3163afa4b1cf789c2c57a2b68e9b
sergsagan <sergey.martynuk@gmail.com>
2026-01-19 14:01:20 +0100

    add sign out

---
+++ pages/signOut.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const authStore = useAuthStore()
+
+onMounted(() => {
+  authStore.signOut()
+})
+</script>
+
+<template>
+  <div class="card bg-base-300 container mt-4 min-h-72 mx-auto flex flex-col justify-center items-center gaap-4">
+    <span class="loading loading-spinner loading-xl" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
