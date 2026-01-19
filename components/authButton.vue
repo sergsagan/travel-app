c65bc8f09d54cff89a04de58fc2364f1d0bab810
Serg Sagan <sergey.martynuk@gmail.com>
2026-01-19 14:04:58 +0100

    add sign out (#32)

--- components/authButton.vue
+++ components/authButton.vue
@@ -3,7 +3,25 @@ const authStore = useAuthStore()
 </script>
 
 <template>
-  <button :disabled="authStore.loading" @click="authStore.signIn" class="btn btn-accent">
+  <div v-if="!authStore.loading && authStore.user" class="dropdown dropdown-end">
+    <div tabindex="0" role="button" class="btn m-1">
+      <div v-if="authStore.user.image" class="avatar">
+        <div class="w-8 rounded-full">
+          <img :src="authStore.user.image"  :alt="authStore.user.name">/>
+        </div>
+      </div>
+      {{ authStore.user.name}}
+    </div>
+    <ul tabindex="0" class="dropdown-content menu bg-base-200 rounded-box z-10 w-46 shadow-sm">
+      <li>
+        <NuxtLink to="/signOut">
+          <Icon name="tabler:logout-2" size="24" />
+          Sign out
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+  <button v-else :disabled="authStore.loading" @click="authStore.signIn" class="btn btn-accent">
     Sign In with
     <span v-if="authStore.loading" class="loading loading-spinner loading-md"></span>
     <Icon v-else name="tabler:brand-github" size="24" />
