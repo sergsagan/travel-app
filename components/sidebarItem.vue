0b5fece9a2f20fbf82248d5d4800919e57352752
sergsagan <sergey.martynuk@gmail.com>
2026-01-23 18:31:55 +0100

    collapsed/expanded state

--- components/sidebarItem.vue
+++ components/sidebarItem.vue
@@ -1,19 +1,22 @@
 <script setup lang="ts">
 const props = defineProps<{
-  label: string
-  icon: string
-  href: string
+  label: string;
+  icon: string;
+  href: string;
+  showLabel: boolean;
 }>();
 const route = useRoute();
 </script>
 
 <template>
-  <NuxtLink
-      :to="props.href"
-      class="flex justify-start gap-2 p-2 hover:bg-base-200 hover:cursor-pointer"
-      :class="{ 'bg-base-200': route.path === props.href }"
-  >
-    <Icon :name="props.icon" size="24" />
-    {{ props.label }}
-  </NuxtLink>
+  <div class="tooltip-right" :data-tip="showLabel ? '' : props.label" :class="{ tooltip: !showLabel }">
+    <NuxtLink
+        :to="props.href"
+        class="flex gap-2 p-2 hover:bg-base-200 hover:cursor-pointer"
+        :class="{ 'bg-base-200': route.path === props.href, 'justify-center': !showLabel, 'justify-start': showLabel }"
+    >
+      <Icon :name="props.icon" size="24" />
+      <span v-if="showLabel">{{ props.label }}</span>
+    </NuxtLink>
+  </div>
 </template>
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,20 +1,21 @@
 <script setup lang="ts">
-
+const isSidebarOpen = ref(true);
 </script>
 
 <template>
   <div class="flex flex-1">
-    <div class="bg-base-100 w-64">
-      <div class="flex justify-end">
-        <Icon name="tabler:chevron-left" size="42" />
+    <div class="bg-base-100" :class="{'w-48': isSidebarOpen, 'w-16': !isSidebarOpen }">
+      <div class="flex p-2 hover:cursor-pointer" :class="{ 'justify-center': !isSidebarOpen, 'justify-end': isSidebarOpen }" @click="isSidebarOpen = !isSidebarOpen">
+        <Icon v-if="isSidebarOpen" name="tabler:square-chevron-left" size="24" />
+        <Icon v-else name="tabler:square-chevron-right" size="24" />
       </div>
       <div class="flex flex-col">
-        <SidebarItem href="/dashboard" label="Locations" icon="tabler:map-2" />
-        <SidebarItem href="/dashboard/add" label="Add Location" icon="tabler:circle-plus-filled" />
+        <SidebarItem :showLabel="isSidebarOpen" href="/dashboard" label="Locations" icon="tabler:map-2" />
+        <SidebarItem :showLabel="isSidebarOpen" href="/dashboard/add" label="Add Location" icon="tabler:circle-plus-filled" />
         <div class="divider" />
-        <SidebarItem href="/signOut" label="Sign Out" icon="tabler:logout-2" />
+        <SidebarItem :showLabel="isSidebarOpen" href="/signOut" label="Sign Out" icon="tabler:logout-2" />
       </div>
     </div>
-    <div class="flex-1"></div>
+    <div class="flex-1" />
   </div>
 </template>
