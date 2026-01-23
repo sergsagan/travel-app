e946813328bca23f38aeda68ecd0f70b1becf7c3
sergsagan <sergey.martynuk@gmail.com>
2026-01-23 12:52:04 +0100

    add sidebar

---
+++ components/sidebarItem.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps<{
+  label: string
+  icon: string
+  href: string
+}>();
+const route = useRoute();
+</script>
+
+<template>
+  <NuxtLink
+      :to="props.href"
+      class="flex justify-start gap-2 p-2 hover:bg-base-200 hover:cursor-pointer"
+      :class="{ 'bg-base-200': route.path === props.href }"
+  >
+    <Icon :name="props.icon" size="24" />
+    {{ props.label }}
+  </NuxtLink>
+</template>
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -3,5 +3,18 @@
 </script>
 
 <template>
-<h1>You are logged in!</h1>
+  <div class="flex flex-1">
+    <div class="bg-base-100 w-64">
+      <div class="flex justify-end">
+        <Icon name="tabler:chevron-left" size="42" />
+      </div>
+      <div class="flex flex-col">
+        <SidebarItem href="/dashboard" label="Locations" icon="tabler:map-2" />
+        <SidebarItem href="/dashboard/add" label="Add Location" icon="tabler:circle-plus-filled" />
+        <div class="divider" />
+        <SidebarItem href="/signOut" label="Sign Out" icon="tabler:logout-2" />
+      </div>
+    </div>
+    <div class="flex-1"></div>
+  </div>
 </template>
