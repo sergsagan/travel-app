0a482ad053ae134ffa96ea2a87e51c4d0d67b040
sergsagan <sergey.martynuk@gmail.com>
2026-03-19 16:38:31 +0100

    temporary close

--- lib/db/schema/locationLog.ts
+++ lib/db/schema/locationLog.ts
@@ -2,7 +2,7 @@ import { int, real, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 import { user } from '../../db/schema/auth';
 import { location } from './location';
-import { relations } from "drizzle-orm";
+//import { relations } from "drizzle-orm";
 
 export const locationLog = sqliteTable('locationLog', {
   id: int().primaryKey({ autoIncrement: true }),
@@ -17,9 +17,9 @@ export const locationLog = sqliteTable('locationLog', {
   updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
 });
 
-export const locationLogRelations = relations(locationLog, ({ one }) => ({
+/*export const locationLogRelations = relations(locationLog, ({ one }) => ({
   location: one(location, {
     fields: [locationLog.locationId],
     references: [location.id],
   }),
-}));
+}));*/
