e9e98e97a2d59d5355ff330c40c9f800cf252d8c
sergsagan <sergey.martynuk@gmail.com>
2026-04-28 13:39:04 +0200

    page displays location log details

--- pages/dashboard/location/[slug]/[id].vue
+++ pages/dashboard/location/[slug]/[id].vue
@@ -1,7 +1,47 @@
 <script setup lang="ts">
+import { toDateTimeLocal } from "~/utils/date";
 
+const route = useRoute();
+const locationsStore = useLocationStore();
+const {
+  currentLocationLog: locationLog,
+  currentLocationLogStatus: status,
+  currentLocationLogError: error
+} = storeToRefs(locationsStore);
+
+const loading = computed(() => status.value === 'pending');
+const errorMessage = computed(() => error.value?.statusMessage);
+
+onMounted(() => {
+  if (typeof route.params.slug === 'string' && typeof route.params.id === 'string') {
+    locationsStore.refreshCurrentLocationLog();
+  }
+});
 </script>
 
 <template>
-  <h2>This is location log</h2>
+  <div class="page-content-top">
+    <div v-if="loading">
+      <div class="loading" />
+    </div>
+    <div v-if="errorMessage && !loading" class="alert alert-error">
+      <h2 class="text-xl">
+        {{ errorMessage }}
+      </h2>
+    </div>
+    <div v-if="route.name === 'dashboard-location-slug-id' && locationLog && !loading">
+      <p class="text-small italic text-gray-500">
+        <span v-if="locationLog.startedAt !== locationLog.endedAt">
+          {{ toDateTimeLocal(locationLog.startedAt) }} / {{ toDateTimeLocal(locationLog.endedAt) }}
+        </span>
+        <span v-else>
+          {{ toDateTimeLocal(locationLog.startedAt) }}
+        </span>
+      </p>
+      <h2 class="text-xl">
+        {{ locationLog.name }}
+      </h2>
+      <p class="text-sm">{{ locationLog.description }}</p>
+    </div>
+  </div>
 </template>
