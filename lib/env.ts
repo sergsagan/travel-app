57b9f0b47828aa69e6f4080e60107ea26d5053a9
Serg Sagan <sergey.martynuk@gmail.com>
2026-05-11 16:32:42 +0200

    041.s3-storage-setup (#63)
    * setup minio in docker container
    
    * update env with s3 credentials and endpoint

--- lib/env.ts
+++ lib/env.ts
@@ -11,6 +11,11 @@ const envSchema = z.object({
   BETTER_AUTH_URL: z.string(),
   AUTH_GITHUB_CLIENT_ID: z.string(),
   AUTH_GITHUB_CLIENT_SECRET: z.string(),
+  S3_ENDPOINT: z.string(),
+  S3_ACCESS_KEY: z.string(),
+  S3_ACCESS_SECRET: z.string(),
+  S3_REGION: z.string(),
+  S3_BUCKET: z.string(),
 });
 
 export type Env = z.infer<typeof envSchema>;
